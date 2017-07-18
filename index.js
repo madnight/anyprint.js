90d25e8fd27518470ac9f5e9b3674216fe139dec
Fabian Beuke <mail@beuke.org>
2017-07-18 23:31:50 +0200

    add index.js and test.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// C
+exports.printf = console.log
+
+// Java
+exports.System = { out: { println: console.log } }
+
+// Ada
+exports.Ada = { Text_IO : { Put_Line: console.log } }
+
+// Go
+exports.fmt = { Println: console.log }
+
+
+
---
+++ test.js
@@ -0,0 +1,8 @@
+var _ = require('./index.js');
+var k;
+for (k in _) global[k] = _[k];
+
+Ada.Text_IO.Put_Line("Hello Ada")
+System.out.println("Hello Java")
+fmt.Println("Hello Go")
+printf("Hello %s\n", "C");

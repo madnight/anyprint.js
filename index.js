6b74a8e25bcb9bd2214a91a8355de1212315d1a3
Fabian Beuke <mail@beuke.org>
2017-07-19 00:08:09 +0200

    add more langs

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
+
+var print = process.stdout.write.bind(process.stdout)
+var printf = console.log
+var println = console.log
+
 // C
-exports.printf = console.log
+exports.printf = printf
+
+// Bash
+exports.echo = println
+
+// B
+exports.putchar = print
+
+// Python
+exports.print = print
+
+// Pascal
+exports.writeLn = print
 
 // Java
-exports.System = { out: { println: console.log } }
+exports.System = {
+    out: {
+        println: println,
+        printf: printf
+    },
+// Visual Basic .NET
+    Console: {
+         Write: print,
+         WriteLine: println,
+    },
+}
+
+// lua
+exports.io = {
+        write: println
+}
 
 // Ada
-exports.Ada = { Text_IO : { Put_Line: console.log } }
+exports.Ada = {
+    Text_IO : {
+        Put_Line: println
+    }
+}
 
 // Go
-exports.fmt = { Println: console.log }
-
-
-
+exports.fmt = {
+    Println: println,
+    Printf: printf,
+    Print: print
+}
--- test.js
+++ test.js
@@ -3,6 +3,21 @@ var k;
 for (k in _) global[k] = _[k];
 
 Ada.Text_IO.Put_Line("Hello Ada")
+
 System.out.println("Hello Java")
-fmt.Println("Hello Go")
-printf("Hello %s\n", "C");
+
+fmt.Println("Hello Go Println")
+fmt.Print("Hello Go Print")
+
+printf("Hello C %s\n", "printf")
+
+echo("Hello Bash echo")
+
+print("Hello Python print")
+
+writeLn("Hello Pascal writeLn")
+
+io.write("Hello Lua io.write")
+
+System.Console.Write("Visual Basic Write")
+System.Console.Write("Visual Basic WriteLine")
